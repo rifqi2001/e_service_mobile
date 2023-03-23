33d54689eb227ac95e6f21708150d188b3ea003f
rifqi2001 <rifqisembada04@gmail.com>
2023-03-23 16:34:29 +0700

    menambahkan splash screen dan menghapus landing

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:e_service/menu.dashboard.dart';
 import 'package:e_service/login.dart';
 import 'package:e_service/register.dart';
-import 'package:e_service/landing.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
   runApp(
     MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: 'landing',
+        initialRoute: 'login',
         theme: ThemeData(
         fontFamily: 'Poppins',
       ),
         routes: {
-          'landing': (context) => const MyLanding(),
           'login': (context) => const MyLogin(),
           'register': (context) => const MyRegister(),
           'menu.dashboard': (context) => const MenuDashboardPage(),
